b057e8b656977453db17f32d32757e9dae99dd33
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-12-18 22:48:27 +0900

    feat: export

--- src/gluon/customauth/index.ts
+++ src/gluon/customauth/index.ts
@@ -11,3 +11,7 @@ export * from "./params_pb";
 export * from "./query_pb";
 export * from "./tx_pb";
 export * from "./tx-builder";
+export * from "../../cosmos/crypto/multisig/v1beta1/multisig_pb";
+export * from "../../cosmos/tx/v1beta1/tx_pb";
+export * from "../../cosmos/base/v1beta1/coin_pb";
+export * from "../../cosmos/tx/signing/v1beta1/signing_pb";
