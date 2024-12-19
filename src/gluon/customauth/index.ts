3a4aa29f45729d06d1803e0879af0beec8a9025c
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-12-19 23:47:13 +0900

    feat: add secp256k1 pubkey

--- src/gluon/customauth/index.ts
+++ src/gluon/customauth/index.ts
@@ -15,3 +15,4 @@ export * from "../../cosmos/crypto/multisig/v1beta1/multisig_pb";
 export * from "../../cosmos/tx/v1beta1/tx_pb";
 export * from "../../cosmos/base/v1beta1/coin_pb";
 export * from "../../cosmos/tx/signing/v1beta1/signing_pb";
+export * from "../../cosmos/crypto/secp256k1/keys_pb";
