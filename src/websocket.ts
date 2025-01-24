6cdca8c0c66d400fd415e78697310e6ade5a6ccd
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2025-01-24 19:51:26 +0800

    client

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,25 +1,56 @@
 import { io, type Socket } from "socket.io-client";
+import {
+  Contract,
+  Order,
+  WebSocketRequest,
+  WebSocketResponse,
+} from "./api-types";
 
 export class WebSocketClient {
   private socket: Socket;
 
-  constructor(baseUrl: string) {
+  constructor(
+    baseUrl: string,
+    onOrdersSnapshot: (orders: Order[]) => Promise<void>,
+    onNewOrder: (order: Order) => Promise<void>,
+    onNewContract: (contract: Contract) => Promise<void>
+  ) {
     this.socket = io(`${baseUrl}/ws`);
 
     this.socket.on("connect", () => {
-      console.log("Connected to server");
+      console.log("Connected to websocket server");
     });
 
-    this.socket.on("message", (data) => {
-      console.log("Received message:", data);
+    this.socket.on("message", async (data: WebSocketResponse) => {
+      if ("orders_snapshot" in data) {
+        await onOrdersSnapshot(data.orders_snapshot);
+      } else if ("new_order" in data) {
+        await onNewOrder(data.new_order);
+      } else if ("new_contract" in data) {
+        await onNewContract(data.new_contract);
+      }
     });
 
     this.socket.on("disconnect", () => {
-      console.log("Disconnected from server");
+      console.log("Disconnected from websocket server");
     });
   }
 
-  async createOrder() {
-    this.socket.emit("message");
+  async createOrder(
+    orderBinary: Uint8Array,
+    pairingId: number,
+    signature: Uint8Array
+  ) {
+    const orderBinaryBase64 = Buffer.from(orderBinary).toString("base64");
+    const signatureBase64 = Buffer.from(signature).toString("base64");
+
+    const req: WebSocketRequest = {
+      new_order: {
+        order_binary: orderBinaryBase64,
+        pairing_id: pairingId,
+        signature: signatureBase64,
+      },
+    };
+    this.socket.emit("message", req);
   }
 }
