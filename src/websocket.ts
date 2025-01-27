3f1b6ee9c86d91f33a4e320acd06ea7f5288bdcf
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2025-01-27 14:50:27 +0800

    remove socket.io

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,33 +1,33 @@
-import { io, type Socket } from "socket.io-client";
 import { Contract, Order, PendingOrder, WebSocketResponse } from "./api-types";
 
 export class WebSocketClient {
-  private socket: Socket;
+  private socket: WebSocket;
 
   constructor(
     baseUrl: string,
-    onConnect: () => Promise<void>,
-    onDisconnect: () => Promise<void>,
+    onOpen: (ev: Event) => Promise<void>,
+    onClose: (ev: CloseEvent) => Promise<void>,
     onOrdersSnapshot: (orders: PendingOrder[]) => Promise<void>,
     onNewOrder: (order: Order) => Promise<void>,
     onNewContract: (contract: Contract) => Promise<void>,
     onOrderCancelled: (orderHash: string) => Promise<void>
   ) {
-    this.socket = io(`${baseUrl}/ws`);
+    this.socket = new WebSocket(`${baseUrl}/ws`);
 
-    this.socket.on("connect", onConnect);
-    this.socket.on("disconnect", onDisconnect);
+    this.socket.onopen = onOpen;
+    this.socket.onclose = onClose;
 
-    this.socket.on("message", async (data: WebSocketResponse) => {
+    this.socket.onmessage = (event: MessageEvent) => {
+      const data = JSON.parse(event.data) as WebSocketResponse;
       if ("orders_snapshot" in data) {
-        await onOrdersSnapshot(data.orders_snapshot);
+        onOrdersSnapshot(data.orders_snapshot);
       } else if ("new_order" in data) {
-        await onNewOrder(data.new_order);
+        onNewOrder(data.new_order);
       } else if ("new_contract" in data) {
-        await onNewContract(data.new_contract);
+        onNewContract(data.new_contract);
       } else if ("order_cancelled" in data) {
-        await onOrderCancelled(data.order_cancelled.order_hash);
+        onOrderCancelled(data.order_cancelled.order_hash);
       }
-    });
+    };
   }
 }
